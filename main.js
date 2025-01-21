5f362256e3fec7876566ffd1557cde9f0b03f3ab
Dhruv02199997 <tusharsharma0219@gmail.com>
2025-01-21 16:08:17 +0530

    add footer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import header from "./src/components/header";
 import fetching_premium_numbers from "./src/components/fetching_premium_numbers.js";
+import footer from "./src/components/footer.js";
 document.getElementById("header").innerHTML = header();
 
 document.getElementById("highlighting-premium-grid").innerHTML =
   fetching_premium_numbers();
+document.getElementById("footer").innerHTML = footer();
---
+++ src/components/footer.js
@@ -0,0 +1,46 @@
+export default function footer() {
+    return`
+    
+    <div class="w-full h-1/2 flex bg-white px-8 py-10">
+        <div class = "flex justify-between flex-col w-full">
+            <div class="">
+                <h3 class = "text-2xl my-2 text-[#474b67] font-bold ">QUICK LINKS</h3>
+                <ul class = "list-none space-y-1">
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Career</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Testimonial</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Video Gallery</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Image Gallery</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Blogs</a></li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h3 class = "text-2xl my-2 text-[#474b67] font-bold ">SUPPORT</h3>
+                <ul class = "list-none space-y-1">
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Contact Us</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">FAQ'S</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300">Why us?</li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h3 class = "text-2xl my-2 text-[#474b67] font-bold ">WE ACCEPT</h3>
+                <ul class = "list-none space-y-1">
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300"><a href="">Terms & conditions</a></li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300">Privcy policy</li>
+                    <li class = "relative w-fit hover:before:w-1/2 before:content-[''] before:absolute before:left-0 before:bottom-0 before:h-[2px] before:w-0 before:bg-blue-500 before:transition-all before:duration-300">Refund policy</li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h3 class = "text-2xl my-2 text-[#474b67] font-bold ">SOCIAL MEDIA</h3>
+                <ul class = "list-none">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                
+            </div>
+        </div>
+    </div>
+    `;
+}
