bf2eb9bb21018433241262987306f0f9640062e6
Prateek <prateeksagar806@gmail.com>
2025-01-21 16:20:42 +0530

    Adding trending numbers section

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import header from "./src/components/header";
 import fetching_premium_numbers from "./src/components/fetching_premium_numbers.js";
+import trending_numbers from "./src/components/trending_numbers.js";
 document.getElementById("header").innerHTML = header();
 
 document.getElementById("highlighting-premium-grid").innerHTML =
   fetching_premium_numbers();
+
+document.getElementById("trending-grid").innerHTML = trending_numbers();
