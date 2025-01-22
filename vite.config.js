347196afb0123e100428b0a7bb0365e0d6525b3f
Prateek <prateeksagar806@gmail.com>
2025-01-22 16:14:44 +0530

    Adding the plugin to adjust the folder structure of the production code

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import customOutputPlugin from "./customOutputPlugin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        about: "about/index.html",
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "src/about/index.html"),
       },
     },
   },
+  plugins: [customOutputPlugin()],
 });
