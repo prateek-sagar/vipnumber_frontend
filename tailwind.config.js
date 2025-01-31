1695390c8087df8bcf36905b5d22af4bbba88d12
Dhruv02199997 <tusharsharma0219@gmail.com>
2025-01-31 18:21:38 +0530

    Create Our partners Slider on home page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll: "scroll 10s linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          "from": { transform: "translateX(-100%)" },
+          "to": { transform: "translateX(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
