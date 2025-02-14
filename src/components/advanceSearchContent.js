8a876aa33e6ec8658fb4f52101dda2c1095db245
Prateek <prateeksagar806@gmail.com>
2025-02-14 12:51:43 +0530

    Create the advance search components

--- src/components/advanceSearchContent.js
+++ src/components/advanceSearchContent.js
@@ -1,19 +1,40 @@
+import close from "../resources/close.png";
 export default function advanceSearchContent() {
   return `
     <div class="fixed h-full w-full top-0 left-0 bg-black bg-opacity-15 backdrop-blur-md z-[99]">
-        <div class="fixed flex rounded-lg flex-col shadow-[0_0_10px_-4px] bg-white p-12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-5">
-            <div class="absolute top-2 right-5 cursor-pointer" id="close-popup-button">x</div>
-            <div class="text-center text-2xl font-semibold">Advance Search</div>
-            <div class="">
-                <div class="">
-                    
+        <div class="fixed flex rounded-[1.25rem] flex-col shadow-[0_0_10px_-4px] bg-white p-12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-3">
+            <div class="absolute top-2 right-2 cursor-pointer" id="close-popup-button">
+            <img class="w-8" src="${close}"/>
+            </div>
+            <div class="text-center text-4xl mb-5 font-semibold">Advance Search</div>
+            <div class="flex flex-col gap-6">
+                <div class="flex">
+                    <div class="w-1/2 relative">
+                        <label class="absolute -top-3 bg-white px-1 font-semibold text-[12px] left-4">Must Contain</label>
+                        <input class=" outline outline-primary outline-[1px] px-4 py-2 rounded-md" type="text" placeholder="e.g. 14,18"/>
+                    </div>
+                    <div class="w-1/2 relative">
+                        <label class="absolute -top-3 bg-white px-1 font-semibold text-[12px] left-4">Not Contain</label>
+                        <input class=" outline outline-primary outline-[1px] px-4 py-2 rounded-md" type="text" placeholder="e.g. 14"/>
+                    </div>
+                </div>
+                <div class="flex">
+                    <div class="w-1/2 relative">
+                        <label class="absolute -top-3 bg-white px-1 font-semibold text-[12px] left-4">Total</label>
+                        <input class=" outline outline-primary outline-[1px] px-4 py-2 rounded-md" type="text" placeholder="e.g. 14,18"/>
+                    </div>
+                    <div class="w-1/2 relative">
+                        <label class="absolute -top-3 bg-white px-1 font-semibold text-[12px] left-4">Sum</label>
+                        <input class=" outline outline-primary outline-[1px] px-4 py-2 rounded-md" type="text" placeholder="e.g. 14"/>
+                    </div>
                 </div>
             </div>
-            <div class="flex  items-center gap-4">
+            <div class="flex items-center gap-4 justify-center ">
                 <div class="w-8 h-[1px] bg-gray-500"></div>
                 OR
                 <div class="w-8 h-[1px] bg-gray-500"></div>
             </div>
+            <div class=" bg-white px-1 font-semibold text-[14px]">Place any number in the available places</div>
             <div class=" flex gap-5">
                 ${numberPlacement()}
             </div>
@@ -27,7 +48,7 @@ export default function advanceSearchContent() {
 
 function places() {
   return `
-    <input type="text" maxlength="1" class="w-10 rounded-lg outline-[1px] outline-primary outline  aspect-square" >
+    <input type="text" maxlength="1" class="w-10 rounded-lg p-2 outline-[1px] outline-primary outline  aspect-square" >
 
     </input>
     `;
